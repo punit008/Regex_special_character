68d8abb57c7b5c3119ea5238acdcf603bfdb3db9
punit008 <74966125+punit008@users.noreply.github.com>
2021-07-13 11:18:01 +0530

    Update regex_specail_character.php

--- regex_specail_character.php
+++ regex_specail_character.php
@@ -5,7 +5,8 @@
 <?php
 $data = "!mlealfM&";
 //$new_data = str_replace  ("'", "", $data);
-$new_data = preg_replace ('/[^\p{L}\p{N}]/u', '/', $data);
+// $new_data = preg_replace ('/[^\p{L}\p{N}]/u', '/', $data);
+echo preg_replace('/([^A-Za-z0-9\s])/', '\\\\$1', $address);
 echo $new_data;
 ?>
 
